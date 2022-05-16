2b3c77983e0daff2474f8035c1099686ef4578b1
nchauhan503 <nchauhan503@gmail.com>
2022-05-15 22:22:44 -0700

    DP2

--- src/com/nitin/BrokenBlocks.java
+++ src/com/nitin/BrokenBlocks.java
@@ -5,120 +5,48 @@ public class BrokenBlocks {
 
     public static void main(String[] args) {
 
-        int lis[][] = {{10,2},{4,2},{-1,-1}};// {{-1,2,3,4},{5,-1,-1,2},{4,3,-1,-1}}; //{{2,5,6},{-1,3,2},{4,-1,5}};
-        System.out.println("Maximum gold coins : " + maxGold(lis));
+        int lis[][] = {{10, 2}, {4, 2}, {-1, -1}};// {{-1,2,3,4},{5,-1,-1,2},{4,3,-1,-1}}; //{{2,5,6},{-1,3,2},{4,-1,5}};
+        System.out.println("Maximum gold coins : " + MaxGold(lis));
     }
 
-    public static int maxGold(int seq[][]) {
 
-        if (seq.length == 0){
-            return 0;
-        }
+    public static int MaxGold(int[][] seq) {
+        int ans = 0;
+        for (int i = 0; i < seq.length; i++) {
 
-        if (seq.length == 1){
-            return findMax(seq[0]);
-        }
+            boolean flag = false;
+            for (int j = 0; j < seq[0].length; j++) {
 
-        int rowToCheck = seq.length - 1;
-        for (int i = 1; i < seq.length ; i++) {
-            boolean blocked = false;
-            if (blocked){
-                break;
-            }
-            int blockedCont = 0;
-            for (int j = 0; j < seq[i].length; j++) {
-                if ( seq[i][j] < 0){
-                    blockedCont++;
-
-                    if (blockedCont == seq[i].length){
-                        blocked = true;
-                        rowToCheck = i-1;
-                        break;
-                    }
+                if (seq[i][j] == -1) {
                     continue;
                 }
-                int maxSum = 0;
-                if ( j - 1 >= 0 && seq[i-1][j-1] > 0){
-                    maxSum = Math.max(seq[i-1][j-1] + seq[i][j],maxSum);
+
+                if (i == 0) {
+                    flag = true;
+                    ans = Math.max(ans, seq[i][j]);
                 }
 
-                if (seq[i-1][j] >= 0){
-                    maxSum = Math.max(seq[i-1][j] + seq[i][j],maxSum);
-                }
+                // -1
+                int a = (i - 1 >= 0) && (j - 1 >= 0) ? seq[i - 1][j - 1] : -1;
 
-                if (j + 1 < seq[i].length && seq[i-1][j+1] > 0){
-                    maxSum = Math.max(seq[i-1][j+1] + seq[i][j],maxSum);
-                }
-                if (maxSum > 0) {
-                    seq[i][j] = maxSum;
-                }
+                // 0
+                int b = (i - 1 >= 0) ? seq[i - 1][j] : -1;
 
+                // +1
+                int c = (j + 1 < seq[i].length) && (i - 1 >= 0) ? seq[i - 1][j + 1] : -1;
+
+                // all the sorrounding values are -1, so no need to update
+                if (a == -1 && b == -1 && c == -1) {
+                    continue;
+                }
+                seq[i][j] = seq[i][j] + Math.max(a, Math.max(b, c));
+                flag = true;
+                ans = Math.max(ans, seq[i][j]);
+            }
+            if (!flag) {
+                return ans;
             }
         }
-        return findMax(seq[rowToCheck]);
+        return ans;
     }
-
-
-    public static int findMax(int lastRow[]){
-        int max = 0;
-        for (int i = 0; i < lastRow.length; i++){
-            if (max < lastRow[i]){
-                max = lastRow[i];
-            }
-        }
-
-        return max;
-    }
-
-    /*public static int subProblem(int row,int seq[][], int col)
-    {
-        int sum0 = 0;
-        int sum1 = 0;
-        int sum2 = 0;
-
-        if (row == seq.length){
-            return 0;
-        }
-
-        int j = col;
-            System.out.println("Elements:"+ seq[row][j]);
-
-        if  ( j - 1 >= 0 && seq[row][j-1] > 0) {
-            System.out.println("1 i: " + row + " ,j:" + j + " ,Elements:"+ seq[row][j]);
-            sum0 =  seq[row][j] + subProblem(row+1,seq,j-1);
-        }
-
-        if ( row+1 < seq.length && seq[row+1][j] > 0){
-            System.out.println("2 i: " + row + " ,j:" + j + " ,Elements:"+ seq[row][j]);
-            sum1 = seq[row][j] + subProblem(row+1,seq,j);
-        }
-
-        if ( j+1 < seq[row].length && row+1 < seq.length && seq[row+1][j+1] > 0){
-            System.out.println("3 i: " + row + " ,j:" + j + " ,Elements:"+ seq[row][j]);
-            sum2 = seq[row][j] + subProblem(row+1,seq,j+1);
-        }
-
-        for (int j = col; j < seq[row].length ; j++) {
-            System.out.println("Elements:"+ seq[row][j]);
-
-            if  ( j - 1 >= 0 && seq[row][j-1] > 0) {
-                System.out.println("1 i: " + row + " ,j:" + j + " ,Elements:"+ seq[row][j]);
-                sum0 =  seq[row][j] + subProblem(row+1,seq,j-1);
-            }
-
-            if ( row+1 < seq.length && seq[row+1][j] > 0){
-                System.out.println("2 i: " + row + " ,j:" + j + " ,Elements:"+ seq[row][j]);
-                sum1 = seq[row][j] + subProblem(row+1,seq,j);
-            }
-
-            if ( j+1 < seq[row].length && row+1 < seq.length && seq[row+1][j+1] > 0){
-                System.out.println("3 i: " + row + " ,j:" + j + " ,Elements:"+ seq[row][j]);
-                sum2 = seq[row][j] + subProblem(row+1,seq,j+1);
-            }
-        }
-
-        System.out.println("Math.max(Math.max(sum0,sum1),sum2):" + Math.max(Math.max(sum0,sum1),sum2));
-        return Math.max(Math.max(sum0,sum1),sum2);
-    }*/
-
 }
