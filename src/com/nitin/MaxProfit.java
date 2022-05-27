c5906101098f5c71a6a591228872bd85af9250e7
nchauhan503 <nchauhan503@gmail.com>
2022-05-26 22:28:09 -0700

    MaxProfit

---
+++ src/com/nitin/MaxProfit.java
@@ -0,0 +1,35 @@
+package com.nitin;
+
+public class MaxProfit {
+
+    public static void main(String[] args) {
+        // write your code here
+        int days[] = {2,4,1};  //{2,4,1}
+        int sum = maxProfit(days); // output should be 55
+
+        System.out.println("Max sum final :" + sum);
+    }
+
+    public static int maxProfit(int[] prices) {
+
+        int profit = 0;
+
+        if (prices.length == 0){
+            return profit;
+        }
+
+        int buy = prices[0];
+
+        for (int i = 0; i < prices.length; i++){
+
+            if ( buy > prices[i] ){
+                buy = prices[i];
+            }
+
+            if ( prices[i] - buy > profit){
+                profit = prices[i] - buy;
+            }
+        }
+        return profit;
+    }
+}
