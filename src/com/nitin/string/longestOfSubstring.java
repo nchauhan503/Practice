1f2e3c9585b93f585da49f18cb1fd5f7de8cb04d
nchauhan503 <nchauhan503@gmail.com>
2022-05-29 10:43:27 -0700

    LongestSubstring

--- src/com/nitin/string/longestOfSubstring.java
+++ src/com/nitin/string/longestOfSubstring.java
@@ -1,25 +1,34 @@
 package com.nitin.string;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class longestOfSubstring {
 
     public static void main(String[] args) {
 
-        //String s = "as"; // Output : 2
-        String s = "asjrgapa"; // Output : 6 (sjrgap)
-        //String s = "dvdf"; // Ouput : 3 (vdf)
-        //String s = " "; // Output : 1
-        //String s = "abcabcbb";  // Output : 3 ("abc" repeating )
+        //String s = "aabaab!bb"; // Output :3 (ab!)
+        //String s = "bbtablud"; //Output : 6 (tablud)
+        //String s = "tmmzuxt"; // Output :5 (mzuxt)
+        //String s = "abba"; // Output :2 ( ab)
+        //String s = "ckilbkd"; // Output :5 ( ilbkd)
+        //String s = "pwwkew" ;// Output :3 ( wke )
+        String s = "aa"; // Output :2
+        //String s = "asjrgapa"; // Output :6 (sjrgap)
+        //String s = "dvdf"; // Ouput :3 (vdf)
+        //String s = " "; // Output :1
+        //String s = "abcabcbb";  // Output :3 ("abc" repeating )
         int l = lengthOfLongestSubstring(s);
         System.out.println("lengthOfLongestSubstring:" + l);
 
     }
 
+
     public static  int lengthOfLongestSubstring(String s) {
 
-        Set<Character> cc = new HashSet<Character>();
+        Map<Character,Integer> cc = new HashMap<Character,Integer>();
 
         int max = 0;
 
@@ -27,20 +36,50 @@ public class longestOfSubstring {
             return 0;
         }
 
-        for (int j = 0; j < s.length(); j++){
-            cc = new HashSet<Character>();
-            cc.add(s.charAt(j));
-            for (int i = j + 1; i < s.length(); i++){
-                char c = s.charAt(i);
-                if (cc.contains(c)){
-                    cc = new HashSet<Character>();
-                }
-                cc.add(c);
-                max = Math.max(max, cc.size());
+        int currStart = 0;
+
+        for (int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if (cc.containsKey(c)) {
+                currStart = Math.max(cc.get(c),currStart);
             }
+            max = Math.max(max, i  + 1 - currStart);
+            cc.put(c,i + 1);
         }
 
-        return Math.max(max,cc.size());
+        return max;
+    }
+
+    public static  int lengthOfLongestSubstringNotAccurate(String s) {
+
+        Map<Character,Integer> cc = new HashMap<Character,Integer>();
+
+        int max = 0;
+
+        if (s == ""){
+            return 0;
+        }
+
+        int curr = 0;
+
+        for (int i = 0; i < s.length(); i++){
+                char c = s.charAt(i);
+                if (cc.containsKey(c)) {
+                    int found = cc.get(c);
+                    if ( i - found < curr) {
+                        curr = curr - found;
+                    }else if (i - found > curr){
+                        curr++;
+                    }
+
+                }else{
+                    curr++;
+                }
+                max = Math.max(max, curr);
+                cc.put(c,i);
+        }
+
+        return max;
     }
 
 
