c3de05028df8c1b6a4f4417d80c7a4718ce421e7
nchauhan503 <nchauhan503@gmail.com>
2022-06-05 21:43:21 -0700

    MeetingRoom2

---
+++ src/com/nitin/interval/MeetingRooms2.java
@@ -0,0 +1,48 @@
+package com.nitin.interval;
+
+import java.util.Arrays;
+
+public class MeetingRooms2 {
+
+    public static void main(String[] args) {
+
+        //int schedule[][] =  {{0,30},{5,10},{7,40},{40,50}};  // Output : false
+
+        int schedule[][] = {{7,10},{2,4}}; // Output : true
+
+        int m =  meetingRoom(schedule);
+
+        System.out.println(" Min no of meeting rooms :" + m);
+
+    }
+
+    public static int meetingRoom(int[][] intervals) {
+
+        Arrays.sort(intervals , (a, b) -> a[0] - b[0]);
+
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+
+        int i = 1;
+        int len = intervals.length;
+
+        int count = 1;
+
+        while ( i < len) {
+
+            int currStart = intervals[i][0];
+            int currEnd = intervals[i][1];
+
+            if ( currStart < end ){
+                count++;
+                start = Math.max(start,currStart);
+                end = Math.min(end,currEnd);
+            }else{
+                end = currEnd;
+            }
+            i++;
+        }
+
+        return count;
+    }
+}
